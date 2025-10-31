df0710447f0c4a9691bc91d3fbac139e22a51f08
wasabi <gordonkao0709@gmail.com>
2025-10-31 16:06:17 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import threading
-from fastapi import FastAPI
-from pydantic import BaseModel
+# from http.server import HTTPServer, SimpleHTTPRequestHandler
+# import threading
+from typing import Annotated, List, Union
+from fastapi import FastAPI, Path, Body, Cookie, Form
+from pydantic import BaseModel, Field
 
 class Item(BaseModel):
     name:str
-    description:str | None = None
-    price: float
-    tax: float | None = None
+    description:str | None = Field(
+        default = None, title = "The description of the item", max_length = 300
+    )
+    price: float = Field(gt = 0, description = "The price must be greater than zero")
+    tax: Union[float, None] = None
+    tags: List[str] = []
 
 app = FastAPI()
 
-
+@app.post("/login")
+async def login(
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()]
+):
+    return {"username": username}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
 
+@app.get("/")
+async def root():
+    return{"message": "Hello World!"}
+
+'''
 @app.get("/tiems/")
 async def read_item(skip: int = 0,limit: int = 10):
     return fake_items_db[skip: skip + limit]
+
 
 fake_items_db = [
     {"item_name": "Foo"},
     {"item_name": "Bar"},
     {"item_name": "Baz"}
 ]
+'''
 
-async def root():
-    return{"message": "Hello World!"}
+@app.get("/items/")
+async def read_items(ads_id: Annotated[str | None, Cookie()]):
+    return {"ads_id": ads_id}
 
 @app.post("/items/")
 async def create_item(item: Item):
@@ -38,12 +55,26 @@ async def create_item(item: Item):
         item_dict.update({"price_with_tax" : price_with_tax})
     return item_dict
 
+'''
 @app.put("/items/{items_id}")
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.model_dump()}
+async def update_item(
+    item_id: Annotated[int, Path(title = "The ID of the item to get", ge = 0, le = 1000)], 
+    item: Item, 
+    q: str | None = None
+):
+    results = {"item_id": item_id}
     if q:
-        result.update({"q": q})  
-    return result
+        results.update({"q": q}) 
+    if item:
+        results.update({"item": item})
+    return results
+'''
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed = True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
 
 
 #------------------server test
